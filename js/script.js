64313effa7ed443598c6d5bc5500ce38be51123a
Piotr <dihake666@gmail.com>
2022-07-21 14:25:55 +0200

    created script for updating the age

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+document.getElementById("age").innerHTML = "Hi, I'm Piotr and I'm " + getAge("2002-09-08") + " years old. I'm a polyglot and a programmer. I want to pursue my carrer in IT with foreign languages further."
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+
